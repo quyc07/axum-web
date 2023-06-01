df72cead84bf8a3f617f71700aecda393615e1e6
quyuanchao <quyuanchao@meituan.com>
2023-06-01 11:49:31 +0800

    simple tonic

--- src/server.rs
+++ src/server.rs
@@ -25,16 +25,3 @@ impl Greeter for MyGreeter {
         Ok(Response::new(reply)) // Send back our formatted greeting
     }
 }
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let greeter = MyGreeter::default();
-
-    Server::builder()
-        .add_service(GreeterServer::new(greeter))
-        .serve(addr)
-        .await?;
-
-    Ok(())
-}
